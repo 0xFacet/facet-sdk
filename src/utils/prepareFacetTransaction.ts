0f6a183c0e3686ad63ba5b69e4424ea7135bb07e
Michael Hirsch <michael@hirsch.me>
2024-10-28 14:31:16 -0500

    fix transaction creation and hash prediction

--- src/utils/prepareFacetTransaction.ts
+++ src/utils/prepareFacetTransaction.ts
@@ -9,7 +9,11 @@ export const prepareFacetTransaction = async (
   account: Account,
   params: FacetTransactionParams
 ) => {
-  const { to, value = 0n, data = "0x" } = params;
+  const { to, value = 0n, data = "0x", extraData } = params;
+
+  if (!facetPublicClient.chain) {
+    throw new Error("Facet chain not found");
+  }
 
   const [estimateFeesPerGas, estimateGas, latestBlock] = await Promise.all([
     facetPublicClient.estimateFeesPerGas({
@@ -31,17 +35,22 @@ export const prepareFacetTransaction = async (
   const attributes = decodeAttributes(latestTransaction.input);
   const { fctMintRate } = attributes;
 
-  const inputCost = calculateInputCost(toBytes(data));
-  const mintAmount = inputCost * fctMintRate;
-
-  const rlpEncodedTransaction = toRlp([
+  const transactionData = [
+    toHex(facetPublicClient.chain.id), // L2 chain id
     to, // L2 recipient address
     toHex(value), // L2 value
     toHex(estimateFeesPerGas?.maxFeePerGas || 0n), // Max fee per gas estimate
     toHex(estimateGas), // Estimated gas limit
     data, // L2 transaction data
-    toHex(mintAmount), // Calculated mint amount
-  ]);
+  ];
+
+  // Extra data to mine more
+  if (extraData) transactionData.push(extraData);
+
+  const encodedTransaction = concatHex([toHex(70), toRlp(transactionData)]);
+
+  const inputCost = calculateInputCost(toBytes(encodedTransaction));
+  const mintAmount = inputCost * fctMintRate;
 
   return {
     values: {
@@ -49,6 +58,6 @@ export const prepareFacetTransaction = async (
       maxFeePerGas: estimateFeesPerGas?.maxFeePerGas,
       gasLimit: estimateGas,
     },
-    encodedTransaction: concatHex([toHex(70), rlpEncodedTransaction]),
+    encodedTransaction,
   };
 };
