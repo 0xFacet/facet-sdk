fe821ccf64aaf5348e7732f770c48c62e437b1e8
Michael Hirsch <michael@hirsch.me>
2024-11-25 12:37:01 -0600

    remove l1txorigin and maxfeecap and make extradata always added to the l1 txn

--- src/utils/encodeDepositTx.ts
+++ src/utils/encodeDepositTx.ts
@@ -2,12 +2,10 @@ import { concatHex, toHex, toRlp } from "viem";
 
 interface DepositTx {
   sourceHash: `0x${string}`;
-  l1TxOrigin: `0x${string}`;
   from: `0x${string}`;
   to: `0x${string}`;
   mint: bigint;
   value: bigint;
-  gasFeeCap: bigint;
   gasLimit: bigint;
   data: `0x${string}`;
 }
@@ -15,12 +13,10 @@ interface DepositTx {
 export const encodeDepositTx = (tx: DepositTx): `0x${string}` => {
   const serializedTransaction = [
     tx.sourceHash,
-    tx.l1TxOrigin,
     tx.from,
     tx.to,
     tx.mint ? toHex(tx.mint) : "0x",
     tx.value ? toHex(tx.value) : "0x",
-    tx.gasFeeCap ? toHex(tx.gasFeeCap) : "0x",
     tx.gasLimit ? toHex(tx.gasLimit) : "0x",
     "0x",
     tx.data,
--- src/utils/prepareFacetTransaction.ts
+++ src/utils/prepareFacetTransaction.ts
@@ -7,22 +7,19 @@ export const prepareFacetTransaction = async (
   l2ChainId: number,
   fctMintRate: bigint,
   params: FacetTransactionParams & {
-    maxFeePerGas: bigint | undefined;
     gasLimit: bigint | undefined;
   }
 ) => {
-  const { to, value, data, extraData, maxFeePerGas, gasLimit } = params;
+  const { to, value, data, extraData, gasLimit } = params;
 
   const transactionData = [
     toHex(l2ChainId),
     to ?? "0x",
     value ? toHex(value) : "0x",
-    maxFeePerGas ? toHex(maxFeePerGas) : "0x",
     gasLimit ? toHex(gasLimit) : "0x",
     data ?? "0x",
+    extraData ?? "0x",
   ];
-
-  if (extraData) transactionData.push(extraData);
 
   const encodedTransaction = concatHex([toHex(70), toRlp(transactionData)]);
 
